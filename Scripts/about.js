63078b4e60a9f05f222d2035a48818c76de5bf36
Bisimtech <simonwaja@gmail.com>
2025-04-25 13:56:53 +0200

    Update about.js

--- Scripts/about.js
+++ Scripts/about.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('click', function(e) {
+    if (!e.target.closest('.mobile-menu') && !e.target.closest('.nav-links')) {
+        navLinks.classList.remove('active');
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+});
