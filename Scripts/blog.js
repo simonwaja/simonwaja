e26cfcb96df1401f4313231830f869fc92cb74ab
Bisimtech <simonwaja@gmail.com>
2025-04-25 13:47:10 +0200

    Update blog.js

--- Scripts/blog.js
+++ Scripts/blog.js
@@ -61,3 +61,11 @@ document.addEventListener('DOMContentLoaded', function() {
       navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
     });
   });
+
+document.addEventListener('click', function(e) {
+    if (!e.target.closest('.mobile-menu') && !e.target.closest('.nav-links')) {
+        navLinks.classList.remove('active');
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+});
